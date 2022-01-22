06a7741e27a68cf6e9cc30f62b676f7ac9981023
Bora Bilir <bora.bilir@outlook.com>
2022-01-23 01:38:08 +0300

    Adding player and camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target, farBackground, middleBackground;
+    [SerializeField] private float minHeight, maxHeight;
+    
+    private Vector3 lastPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float clampedY = Mathf.Clamp(target.position.y, minHeight, maxHeight);
+        
+        transform.position = new Vector3(target.position.x, clampedY, transform.position.z);
+
+        Vector3 amountToMove = transform.position - lastPosition;
+        farBackground.position += amountToMove;
+        middleBackground.position += amountToMove * .5f;
+
+        lastPosition = transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 0;
+    [SerializeField] private float jumpForce = 0;
+    [SerializeField] private LayerMask groundLayers = new LayerMask();
+
+    private bool isGrounded = false;
+    private bool canDoubleJump = true;
+    private Rigidbody2D playerBody;
+    private Transform groundCheckPoint;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        groundCheckPoint = transform.GetChild(0).transform;
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        playerBody.velocity = new Vector2(moveSpeed * horizontalInput, playerBody.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, groundLayers);
+
+        if (isGrounded)
+            canDoubleJump = true;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                playerBody.velocity = new Vector2(playerBody.velocity.x, jumpForce);
+                //playerBody.AddForce(new Vector2(playerBody.velocity.x, jumpForce*Time.deltaTime));
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    playerBody.velocity = new Vector2(playerBody.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+        }
+
+        if (playerBody.velocity.x > 0)
+            spriteRenderer.flipX = false;
+        else if(playerBody.velocity.x < 0)
+            spriteRenderer.flipX = true;
+
+
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("moveSpeed", Mathf.Abs(playerBody.velocity.x));
+    }
+}
